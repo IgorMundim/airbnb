a74e5743a323aa2a59dedf602557294be4b91f1d
Igor Mundim <mundim.github@gmail.com>
2023-08-28 10:08:00 -0300

    create navbar and auth ui

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+const Logo = () => {
+  return(
+    <Image 
+      alt="Logo"
+      className="hidden md:block cursor-point"
+      height={100}
+      width={100}
+      src="/images/logo.png"
+    />
+  )
+}
+export default Logo;
